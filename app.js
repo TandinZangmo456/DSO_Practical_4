89824b646539b1116ce15f2c98d0c8095a8e6368
TandinZangmo <02230305.cst@rub.edu.bt>
2025-05-30 18:23:53 +0600

    F

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const port = 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World from Jenkins!');
 });
 
+let server;
+
 if (require.main === module) {
-  app.listen(4000, () => {
-    console.log(`App listening at http://localhost:4000`);
+  server = app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
   });
 }
 
-module.exports = app;
+module.exports = { app, server };
